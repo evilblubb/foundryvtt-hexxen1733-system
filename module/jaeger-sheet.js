ab5f6c0e355cda5f57c7b53b86ac2f978a66db0f
Martin <martin@brunninger.net>
2020-05-11 12:36:44 +0200

    BUGFIX: Werteberechnung INI/PW/AP

--- module/jaeger-sheet.js
+++ module/jaeger-sheet.js
@@ -59,8 +59,8 @@ class JaegerSheet extends ActorSheet {
     }
     
     // FIXME Teile des Codes nach Actor verschieben
-    data.data.calc = data.calc || {};
-    data.data.calc.ini = data.data.attributes.SIN + data.data.attributes.GES + data.data.skills["Reflexe"].value;    
+    data.data.calc = data.data.calc || {};
+    data.data.calc.ini = data.data.attributes.SIN.value + data.data.attributes.GES.value + data.data.skills["Reflexe"].value;    
     data.data.calc.pw = data.data.calc.pw || 1;    
     data.data.calc.ap = 6 - data.data.calc.pw;    
     
