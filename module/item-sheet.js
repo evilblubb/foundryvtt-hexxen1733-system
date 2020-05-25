b098c411dbf9374cfbca51a6b23a0e66c00eb695
Martin <martin@brunninger.net>
2020-05-25 10:32:28 +0200

    Item Sheet um item-type Prüfungen ergänzt, damit es bei anderen Typen als "item" keine Probleme verursacht.

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -20,10 +20,14 @@ class SimpleItemSheet extends ItemSheet {
   /** @override */
   getData() {
     const data = super.getData();
-    data.dtypes = ["String", "Number", "Boolean"];
-    for ( let attr of Object.values(data.data.attributes) ) {
-      attr.isCheckbox = attr.dtype === "Boolean";
+
+    if ("item" === data.type) {
+      data.dtypes = ["String", "Number", "Boolean"];
+      for ( let attr of Object.values(data.data.attributes) ) {
+        attr.isCheckbox = attr.dtype === "Boolean";
+      }
     }
+
     return data;
   }
 
@@ -47,8 +51,10 @@ class SimpleItemSheet extends ItemSheet {
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
 
-    // Add or Remove Attribute
-    html.find(".attributes").on("click", ".attribute-control", this._onClickAttributeControl.bind(this));
+    if ("item" === this.object.data.type) {
+      // Add or Remove Attribute
+      html.find(".attributes").on("click", ".attribute-control", this._onClickAttributeControl.bind(this));
+    }
   }
 
   /* -------------------------------------------- */
@@ -88,26 +94,28 @@ class SimpleItemSheet extends ItemSheet {
   /** @override */
   _updateObject(event, formData) {
 
-    // Handle the free-form attributes list
-    const formAttrs = expandObject(formData).data.attributes || {};
-    const attributes = Object.values(formAttrs).reduce((obj, v) => {
-      let k = v["key"].trim();
-      if ( /[\s\.]/.test(k) )  return ui.notifications.error("Attribute keys may not contain spaces or periods");
-      delete v["key"];
-      obj[k] = v;
-      return obj;
-    }, {});
-    
-    // Remove attributes which are no longer used
-    for ( let k of Object.keys(this.object.data.data.attributes) ) {
-      if ( !attributes.hasOwnProperty(k) ) attributes[`-=${k}`] = null;
-    }
+    if ("item" === this.object.data.type) {
+      // Handle the free-form attributes list
+      const formAttrs = expandObject(formData).data.attributes || {};
+      const attributes = Object.values(formAttrs).reduce((obj, v) => {
+        let k = v["key"].trim();
+        if ( /[\s\.]/.test(k) )  return ui.notifications.error("Attribute keys may not contain spaces or periods");
+        delete v["key"];
+        obj[k] = v;
+        return obj;
+      }, {});
+      
+      // Remove attributes which are no longer used
+      for ( let k of Object.keys(this.object.data.data.attributes) ) {
+        if ( !attributes.hasOwnProperty(k) ) attributes[`-=${k}`] = null;
+      }
 
-    // Re-combine formData
-    formData = Object.entries(formData).filter(e => !e[0].startsWith("data.attributes")).reduce((obj, e) => {
-      obj[e[0]] = e[1];
-      return obj;
-    }, {_id: this.object._id, "data.attributes": attributes});
+      // Re-combine formData
+      formData = Object.entries(formData).filter(e => !e[0].startsWith("data.attributes")).reduce((obj, e) => {
+        obj[e[0]] = e[1];
+        return obj;
+      }, {_id: this.object._id, "data.attributes": attributes});
+    }
 
     // Update the Item
     return this.object.update(formData);
