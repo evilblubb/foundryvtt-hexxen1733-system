21079aa8f64971eac1cacb09cb81804b0de97c21
Martin <martin@brunninger.net>
2020-07-09 16:47:33 +0200

    DB-Konverterscript um Zuordnung Jägerkräfte zu den Rollen/Professionen erweitert.

--- raw/convert.js
+++ raw/convert.js
@@ -17,6 +17,41 @@ function generateID(count, k) {
   return str;
 }
 
+const powers = JSON.parse(fs.readFileSync(`${__dirname}/packs/skills.json`, "utf8"));
+function getPowers(type, role) {
+  const out = [];
+  const set = powers[type][role];
+  if (! set) {
+    console.error(`Jägerkräfte für Rolle ${role} nicht gefunden!`);
+    return [];
+  }
+  new Map(Object.entries(set)).forEach((value, key) => {
+    new Map(Object.entries(value)).forEach(value => {
+      const power = { name: value.name, type: key };
+      if ("aufbau" === key) {
+        power.type = "ausbau"; // TODO: Fix für skills.json
+        power.features = [];
+        const stammeffekt = value.stammeffekt;
+        power.features.push({name: stammeffekt.name, type: "stammeffekt"});
+        const features = ["gesellen", "expert", "meister"];
+        features.forEach(feature => {
+          new Map(Object.entries(value[feature])).forEach(value => {
+            let type = "";
+            switch (feature) { // TODO: Fix für skills.json
+              case "gesellen": type = "geselle"; break;
+              case "expert": type = "experte"; break;
+              default: type = feature;
+            }
+            power.features.push({name: value.name, type: type});
+          });
+        });
+      }
+      out.push(power);
+    });
+  });
+  return out;
+}
+
 const list = ["motivation", "role", "profession"];
 for (const key in list) {
   if (list.hasOwnProperty(key)) {
@@ -25,16 +60,17 @@ for (const key in list) {
     console.log(`Converting ${type} ...`);
     const content = fs.readFileSync(`${__dirname}/packs/${type}.json`, "utf8");
     const data = JSON.parse(content);
-    
+
     let fd;
     try {
       fd = fs.openSync(`${__dirname}/../packs/${type}.db`, 'w'); // alte Datei überschreiben
-    
+
       let i = 0;
       for (const key in data) {
         if (data.hasOwnProperty(key)) {
           const item = data[key];
-      
+          console.log(`Converting   ${item.name} ...`);
+
           const out = {};
           out._id = generateID(16);
           out.name = item.name;
@@ -44,22 +80,26 @@ for (const key in list) {
           out.data.references = item.references;
           out.data.description = item.description;
           out.data.summary = item.summary;
+          if ("role" === type || "profession" === type) {
+            out.data.powers = getPowers(type, key);
+          }
           out.flags = {};
           out.img = "systems/hexxen-1733/img/Siegel-Rabe-small.png";
-      
+
           fs.appendFileSync(fd, JSON.stringify(out), 'utf8');
           fs.appendFileSync(fd, '\n', 'utf8');
         }
       }
-    
+
     } catch (err) {
       /* Handle the error */
-      console.log(err);
+      console.error(err);
     } finally {
       if (fd !== undefined)
         fs.closeSync(fd);
     }
-    
+
   }
 }
 
+console.log(`Done.`);
