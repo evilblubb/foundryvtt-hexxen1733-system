aae6102523d460b2224ff2d40d59988469827637
Martin <martin@brunninger.net>
2020-07-20 12:54:22 +0200

    DBs: Konvertierungsskript um ID-Prüfung erweitert, (alte) IDs in Rollen hinzugefügt und role.db neu erzeugt.

--- raw/convert.js
+++ raw/convert.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const generated = [];
+const generatedIDs = [];
 function generateID(count, k) {
   const _sym = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
   let str;
@@ -11,10 +11,36 @@ function generateID(count, k) {
     for(let i = 0; i < count; i++) {
         str += _sym[Math.floor(Math.random() * (_sym.length))];
     }
-  } while (generated.includes(str));
+  } while (generatedIDs.includes(str));
 
-  generated.push(str);
+  generatedIDs.push(str);
   return str;
+}
+
+function checkIDs(data, path="") {
+  let count = 0;
+  if (data.content) data = data.content;
+
+  // TODO: Sonderfallbehandlung "powers": 2 Hauptkategorien
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const value = data[key];
+      if (value.hasOwnProperty("_id")) {
+        const id = value._id;
+        if (generatedIDs.includes(id)){
+          delete value._id;
+          count++;
+          console.warn(`  @_ID\:${path}/${key}: Duplicate id found! Will be randomly created!`);
+        }
+        generatedIDs.push(id);
+      } else {
+        count++;
+        console.warn(`  @_ID\:${path}/${key}: No id found! Will be randomly created!`);
+      }
+    }
+  }
+  return count;
 }
 
 function checkTodo(data, path="") {
@@ -62,12 +88,43 @@ function _findRefs(data, path="") {
   }
 }
 
+function convertItem(key, type, item) {
+  const out = {};
+  out._id = item._id || generateID(16);
+  out.name = item.name;
+  out.permission = { "default": 0 };
+  out.type = type;
+  out.data = {};
+  out.data.references = item.references || [];
+  out.data.description = item.description || "";
+  out.data.summary = item.summary || "";
+  out.flags = {};
+  out.img = "systems/hexxen-1733/img/Siegel-Rabe-small.png";
+
+  if ("role" === type) {
+    _convertRoleItem(key, type, item, out);
+  } else if ("profession" === type) {
+    _convertProfessionItem(key, type, item, out);
+  }
+
+  return out;
+}
+
+function _convertRoleItem(key, type, item, out) {
+  out.data.create = item.create || "";
+  out.data.powers = getPowers(type, key);
+}
+
+function _convertProfessionItem(key, type, item, out) {
+  out.data.powers = getPowers(type, key);
+}
+
 function getPowers(type, role) {
-  const powers = input.skills;
+  const powers = input.power.content;
   const out = [];
   const set = powers[type][role];
   if (! set) {
-    console.error(`Jägerkräfte für Rolle ${role} nicht gefunden!`);
+    console.error(`  Jägerkräfte für Rolle ${role} nicht gefunden!`);
     return [];
   }
   new Map(Object.entries(set)).forEach((value, key) => {
@@ -97,53 +154,40 @@ function getPowers(type, role) {
   return out;
 }
 
-function convertItem(key, type, item) {
-  const out = {};
-  out._id = generateID(16);
-  out.name = item.name;
-  out.permission = { "default": 0 };
-  out.type = type;
-  out.data = {};
-  out.data.references = item.references || [];
-  out.data.description = item.description || "";
-  out.data.summary = item.summary || "";
-  out.flags = {};
-  out.img = "systems/hexxen-1733/img/Siegel-Rabe-small.png";
-
-  if ("role" === type) {
-    _convertRoleItem(key, type, item, out);
-  } else if ("profession" === type) {
-    _convertProfessionItem(key, type, item, out);
-  }
-
-  return out;
-}
-
-function _convertRoleItem(key, type, item, out) {
-  out.data.create = item.create || "";
-  out.data.powers = getPowers(type, key);
-}
-
-function _convertProfessionItem(key, type, item, out) {
-  out.data.powers = getPowers(type, key);
-}
-
 /********************
  * main code        *
  ********************/
-
- // preload files
-const filesIn = { "motivation": "motivation", "skills": "power", "role": "role", "profession": "profession" };
+const filesIn = {
+  "motivation": "motivation.json",
+  "power": "skills.json",
+  "role": "role.json",
+  "profession": "profession.json"
+};
+const filesOut = {
+  "motivation": "motivation.db",
+  // "power": "skills.db",
+  "role": "role.db",
+  "profession": "profession.db"
+};
 const input = {};
+const output = {};
 let error = false;
+
+// preload files
 for (const key in filesIn) {
   if (filesIn.hasOwnProperty(key)) {
-    const file = key;
+    const type = key;
+    const file = filesIn[type];
 
-    console.log(`Loading ${file}.json ...`);
+    console.log(`Loading ${file} ...`);
     try {
-      const data = JSON.parse(fs.readFileSync(`${__dirname}/packs/${file}.json`, "utf8"));
-      input[file] = data;
+      const data = JSON.parse(fs.readFileSync(`${__dirname}/packs/${file}`, "utf8"));
+      if (data.content) {
+        input[type] = data;
+      } else {
+        input[type] = {};
+        input[type].content = data;
+      }
     }
     catch (err) {
       console.error(err);
@@ -159,15 +203,23 @@ if (error) {
 // validate files
 for (const key in input) {
   if (input.hasOwnProperty(key)) {
-    const file = key;
-    const data = input[key].content || input[key];
-    const type = filesIn[key];
+    const type = key;
+    const file = filesIn[type];
+    const data = input[type].content;
 
-    console.log(`Validating ${file}.json ...`);
-    const todo = checkTodo(data, `${file}.json`);
-    const refs = checkRefs(data, `${file}.json`);
+    console.log(`Validating ${type} ...`);
+    const ids = checkIDs(data, file);
+    const todo = checkTodo(data, file);
+    const refs = checkRefs(data, file);
     // TODO: weitere Prüfungen auf Vollständigkeit
-    if (todo > 0) console.log(`  ${todo} TODO(s) found!`);
+    if (ids > 0) {
+      input[type].ids = ids;
+      console.warn(`  Missing ${ids} IDs!`);
+    }
+    if (todo > 0) {
+      input[type].todo = todo;
+      console.log(`  ${todo} TODO(s) found!`);
+    }
   }
 }
 if (error) {
@@ -178,30 +230,53 @@ if (error) {
 // convert files
 for (const key in input) {
   if (input.hasOwnProperty(key)) {
-    const file = key;
-    const data = input[key].content || input[key];
-    const type = filesIn[key];
+    const type = key;
+    const data = input[type].content;
 
-    if ("power" === type) continue; // TODO: vorübergehend noch keine DB erzeugen
+    if ("power" === type) {
+      console.log("Skipping powers.");
+      continue; // TODO: vorübergehend noch nicht konvertieren
+    }
 
+    output[type] = {}
+    const content = [];
     console.log(`Converting ${type} ...`);
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        const item = data[key];
+
+        console.log(`  Converting ${item.name} ...`);
+        const out = convertItem(key, type, item);
+        content.push(out);
+      }
+    }
+    output[type].content = content;
+    if (input[type].todo) output[type].todo = input[type].todo;
+    if (input[type].ids) output[type].ids = input[type].ids;
+  }
+}
+
+// create db files
+for (const key in output) {
+  if (input.hasOwnProperty(key)) {
+    const type = key;
+    const file = filesOut[type] || null;
+    const data = output[type].content;
+
+    if (!file) {
+      console.log(`Skipping ${file}.`);
+      continue;
+    }
+
+    console.log(`Creating ${file} ...`);
     let fd;
     try {
       fd = fs.openSync(`${__dirname}/../packs/${type}.db`, 'w'); // alte Datei überschreiben
 
-      let i = 0;
-      for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-          const item = data[key];
-          console.log(`Converting   ${item.name} ...`);
-
-          const out = convertItem(key, type, item);
-
-          fs.appendFileSync(fd, JSON.stringify(out), 'utf8');
-          fs.appendFileSync(fd, '\n', 'utf8');
-        }
-      }
-
+      data.forEach(item => {
+        fs.appendFileSync(fd, JSON.stringify(item), 'utf8');
+        fs.appendFileSync(fd, '\n', 'utf8');
+      });
     } catch (err) {
       /* Handle the error */
       console.error(err);
@@ -209,7 +284,13 @@ for (const key in input) {
       if (fd !== undefined)
         fs.closeSync(fd);
     }
-
+    console.log(`  ${data.length} entries created.`);
+    if (output[type].ids) {
+      console.warn(`  Missing ${output[type].ids} IDs! Created new random IDs!`)
+    }
+    if (output[type].todo) {
+      console.info(`  ${output[type].todo} TODOs pending!`)
+    }
   }
 }
 
